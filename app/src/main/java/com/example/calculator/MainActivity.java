84ada18fd3bae4aa43a55b67ada65a09fab1bac8
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-10-25 17:54:32 +0200

    make all btn

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,150 @@
 package com.example.calculator;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnAdd, btnDiv, btnMal, btnMin, btnEqual, btnAc, btnDel, btnDot;
+    private TextView textViewResult, textViewHistory;
+    private String number = null, status = null;
+    private double firstNum = 0, lastNum = 0;
+    boolean operator = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponent();
+        btnClick(btn0);
+        btnClick(btn1);
+        btnClick(btn2);
+        btnClick(btn3);
+        btnClick(btn4);
+        btnClick(btn5);
+        btnClick(btn6);
+        btnClick(btn7);
+        btnClick(btn8);
+        btnClick(btn9);
+        btnClick(btnDot);
+        btnAdd.setOnClickListener(view -> checkOperation("addition"));
+        btnDiv.setOnClickListener(view -> checkOperation("division"));
+        btnMin.setOnClickListener(view -> checkOperation("subtraction"));
+        btnMal.setOnClickListener(view -> checkOperation("multiplication"));
+
+
     }
+
+    //init all btn and text view
+    private void initComponent() {
+
+        btn0 = findViewById(R.id.btn_0);
+        btn1 = findViewById(R.id.btn_1);
+        btn2 = findViewById(R.id.btn_2);
+        btn3 = findViewById(R.id.btn_3);
+        btn4 = findViewById(R.id.btn_4);
+        btn5 = findViewById(R.id.btn_5);
+        btn6 = findViewById(R.id.btn_6);
+        btn7 = findViewById(R.id.btn_7);
+        btn8 = findViewById(R.id.btn_8);
+        btn9 = findViewById(R.id.btn_9);
+        btnAdd = findViewById(R.id.btn_add);
+        btnDiv = findViewById(R.id.btn_divide);
+        btnMal = findViewById(R.id.btn_multi);
+        btnMin = findViewById(R.id.btn_minus);
+        btnEqual = findViewById(R.id.btn_equal);
+        btnAc = findViewById(R.id.btn_AC);
+        btnDel = findViewById(R.id.btn_del);
+        btnDot = findViewById(R.id.btn_dot);
+        textViewResult = findViewById(R.id.txt_view_result);
+        textViewHistory = findViewById(R.id.txt_view_history);
+
+    }
+
+    //
+    private void numberClick(String number) {
+        if (this.number == null) {
+            this.number = number;
+        } else {
+            this.number += number;
+        }
+        textViewResult.setText(this.number);
+        operator = true;
+
+    }
+
+    private void btnClick(Button btn) {
+        btn.setOnClickListener(view -> numberClick(btn.getText().toString()));
+    }
+
+    private void plus() {
+        lastNum = Double.parseDouble(textViewResult.getText().toString());
+        firstNum += lastNum;
+        textViewResult.setText(Double.toString(firstNum));
+        operator = true;
+    }
+
+    private void minus() {
+        if (firstNum == 0)
+            firstNum = Double.parseDouble(textViewResult.getText().toString());
+        else {
+            lastNum = Double.parseDouble(textViewResult.getText().toString());
+            firstNum -= lastNum;
+        }
+        textViewResult.setText(Double.toString(firstNum));
+        operator = true;
+    }
+
+    private void multiply() {
+        if (firstNum == 0) {
+            firstNum = 1;
+        }
+        lastNum = Double.parseDouble(textViewResult.getText().toString());
+        firstNum *= lastNum;
+        textViewResult.setText(Double.toString(firstNum));
+        operator = true;
+
+    }
+
+    private void divide() {
+        lastNum = Double.parseDouble(textViewResult.getText().toString());
+        if (firstNum == 0) {
+            firstNum = lastNum;
+        } else {
+
+            firstNum /= lastNum;
+        }
+
+        textViewResult.setText(Double.toString(firstNum));
+        operator = true;
+
+    }
+
+    private void checkOperation(String operation) {
+        status = operation;
+        if (operator) {
+            switch (status) {
+                case "multiplication":
+                    multiply();
+                    break;
+                case "division":
+                    divide();
+                    break;
+                case "addition":
+                    plus();
+                    break;
+                case "subtraction":
+                    minus();
+                    break;
+            }
+            status = operation;
+            operator = false;
+            number = null;
+        }
+
+    }
+
+
 }
